e124f41a1632b1bd03f6a3ec3b666ca002f868b1
akarabelyov <akarabelyov@vmware.com>
2020-07-09 23:18:14 +0300

    Add bitly link

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_data_from_file=0
 title="Левски - Локомотив Пловдив"
 subtitle="12.07 21:00"
 link_url=eventim_url
-link_image="https://levski.bg/uploads/news/%D0%B2%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D0%BD%D0%B8_%D0%B1%D0%B8%D0%BB%D0%B5%D1%82%D0%B8_%D0%BB%D0%BE%D0%BA%D0%BE.png"
+link_image="https://bit.ly/300alRM"
 
 token="3411E5982F6B6B6B5F6B6B6B44264639636B626BF7524C508632E70775070CD00EA999D38689A1B1C1D792FAFCCD108D6BB1EE491455799A01BA3050C79FD0F5DEDC2EB2D5A19B37"
 
