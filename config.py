6fdd2b408351443ddc718d7d1e07dc1c17de9b23
akarabelyov <akarabelyov@vmware.com>
2020-08-04 15:46:16 +0300

    Beroe config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 publish=1
-times=0
+times=30
 sleep_time=10
-eventim_url="https://www.eventim.bg/bg/bileti/levski-lokomotiv-plovdiv-sofia-vivacom-arena-georgi-asparuhov-1204260/performance.html"
+eventim_url="https://www.eventim.bg/bg/bileti/levski-beroe-sofia-vivacom-arena-georgi-asparuhov-1207207/performance.html"
 load_data_from_file=0
-title="Левски - Локомотив Пловдив"
-subtitle="12.07 21:00"
-link_url="https://bit.ly/2Dl6KWw"
-link_image="https://bit.ly/300alRM"
+title="Левски - Берое"
+subtitle="10.08 20:00"
+link_url="https://www.eventim.bg/bg/bileti/levski-beroe-sofia-vivacom-arena-georgi-asparuhov-1207207/performance.html"
+link_image="https://bit.ly/33pGe9F"
 
-token="3411E5982F6B6B6B5F6B6B6B44264639636B626BF7524C508632E70775070CD00EA999D38689A1B1C1D792FAFCCD108D6BB1EE491455799A01BA3050C79FD0F5DEDC2EB2D5A19B37"
+token="97657EA42F6B6B6B5F6B6B6BF1E1F79D636B626B084D3A225A8F0011C6626014D7772AF17EA272BC6A517C3F9A690E03575AB97FB0AE469348FDFEA2D24AA3E05B49F504D47BD812"
 
-event_id=1204260
+event_id=1207207
